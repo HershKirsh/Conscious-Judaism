38545f5743d55b71b0cb61224c6c6b7aa102bb28
HershKirsh <hershkirsh@gmail.com>
2020-01-15 14:49:05 -0500

    some improvments

--- js/view.js
+++ js/view.js
@@ -3,19 +3,21 @@ var htmlElements = {
     mainMenu: document.getElementById('main-menu')
 };
 
-const createHtml = (tagName, className, idName, innerContent, insertTo) => {
+const createHtml = (tagName, className, idName, innerContent, insertTo, attribute) => {
     const htmlListItem = document.createElement(tagName);
     if (className) { htmlListItem.classList.add(className) };
     if (idName) { htmlListItem.id = idName };
+    console.log(attribute)
+    if (attribute) { htmlListItem.setAttribute(attribute.name, attribute.value) }
     const textNode = (innerContent);
     htmlListItem.innerHTML = textNode;
     insertTo.insertAdjacentElement('beforeend', htmlListItem);
 };
 const audioSections = {
-    IDs: ['DT', 'DME', 'CIN'],
-    headers: ['Conscious Judaism Series', 'Conscious Chassidus Series', 'Conscious In Nature'],
+    IDs: ['DT', 'DME', 'CIN', 'HEB'],
+    headers: ['Conscious Judaism Series', 'Conscious Chassidus Series', 'Conscious In Nature', 'נשמת ישראל'],
     descriptions: ['Recordings on Sefer Dover Tzedek from Reb Tzodok HaCohen of Lublin',
-        'Recordings on Sefer Degel Machaneh Ephraim', 'Insights conveyed while surrounded by nature'],
+        'Recordings on Sefer Degel Machaneh Ephraim', 'Insights conveyed while surrounded by nature', 'שיעורים בעברית'],
     create: function (data) {
         this.IDs.forEach((item, i) => {
             let innerString = `<div class="subheader-wrapper">
@@ -35,10 +37,10 @@ var audioListItems = {
     listItemNum: 0,
     appendList: function (list, num) {
         list.forEach((item) => {
-            const innerString = `<h3>${item.title}</h3>
+            const innerString = `<h3>${item.number + 1}. ${item.title}</h3><span onclick="nowPlaying.stop(${this.listItemNum})" title="stop playing">X</span>
             <div class="audio-wrapper">
-                <audio onplay="nowPlaying.createElement(${this.listItemNum})" type="audio/mpeg"
-                    src="https://conscious-j.herokuapp.com/${item.audioLink}" controls controlsList="nodownload">
+                <audio onplay="nowPlaying.setPlay(${this.listItemNum})" type="audio/mpeg"
+                    src="https://consciousj.s3.us-east-2.amazonaws.com/audio/${item.title}.mp3" controls controlsList="nodownload">
                     Your browser does not support the audio element.
                 </audio>
                 <div id="speed-wrapper">
@@ -46,15 +48,16 @@ var audioListItems = {
                     <input type="range" id="a" max="2.5" min="0.5" step="0.25" value="1" oninput="adjustSpeed(this, ${this.listItemNum})">
                     <br>x<output name="x" for="a">1</output>
                 </div>
-                <a class="btn yt" href="${item.ytLink}"
+                <a class="btn yt" href="https://www.youtube.com/watch?v=${item.ytId}"
                     title="Watch this recordind on YouTube" target="blank"><i class="fab fa-youtube"></i></a>
-                <a class="btn" href="https://conscious-j.herokuapp.com/recording/download?path=${item.audioLink}" title="Download" download>&#8681;</a>
+                <a class="btn" href="https://consciousj.s3.us-east-2.amazonaws.com/audio/${item.title}.mp3" title="Download" download>&#8681;</a>
             </div>`;
-            createHtml('div', 'list-item', '', innerString, htmlElements.section[num]);
+            createHtml('div', 'list-item', '', innerString, htmlElements.section[num], { name: "tabindex", value: this.listItemNum });
             this.listItemNum++;
         });
         htmlElements.audio = document.querySelectorAll('audio');
         htmlElements.output = document.querySelectorAll('output');
+        htmlElements.listItem = document.querySelectorAll('.list-item');
     },
 };
 
@@ -70,55 +73,51 @@ function adjustSpeed(range, num) {
 
 
 
-var nowPlaying = {
+const nowPlaying = {
     number: 0,
-    createElement: function (num) {
-        this.number = num;
-        let innerString = `<h3>${audioListItems.recordingList[this.number].title}</h3>
-        <div class="audio-wrapper">
-        <div id="speed-wrapper">
-            <label for="a">Playback speed</label><br>
-            <input type="range" id="a" max="2.5" min="0.5" step="0.25" value="1" oninput="adjustSpeed(this, ${this.number})">
-            <br>x<output name="x" for="a">1</output>
-        </div>
-        <button id="pause-btn" title="pause" onclick="nowPlaying.pausePlay(this, ${this.number})"><i class="far fa-pause-circle"></i></button>
-        <input type="range" id="a" max="1" min="0" step="0.1" value="${htmlElements.audio[this.number].volume}" oninput="nowPlaying.volumeChange(this, ${this.number})">
-        <a class="btn yt" href="${audioListItems.recordingList[this.number].ytLink}"
-            title="Watch this recordind on YouTube" target="blank"><i class="fab fa-youtube"></i></a>
-        </div>
-        </div>`;
-        createHtml('div', '', 'nowPlaying', innerString, htmlElements.container);
-    },
-    playing: true,
-    volumeChange: (range) => htmlElements.audio[this.number].volume = range.value,
-    pausePlay: function (btn, num) {
+    playing: false,
+    setPlay: function (num) {
         if (this.playing) {
-            btn.innerHTML = '&#x25B6;';
-            btn.title = 'play';
-            htmlElements.audio[num].pause();
-            this.playing = false
-        } else {
-            btn.innerHTML = '<i class="far fa-pause-circle"></i>';
-            btn.title = 'pause';
-            htmlElements.audio[num].play();
-            this.playing = true;
+            this.stop(this.number);
         }
+        this.number = num;
+        this.playing = true;
+        htmlElements.listItem[num].classList.add('now-playing');
+    },
+    stop: function (num) {
+        htmlElements.listItem[num].classList.remove('now-playing');
+        htmlElements.audio[num].pause();
     }
 };
-var heros = document.querySelectorAll('.hero');
-let scale = 100;
-var slide = 0;
-window.addEventListener('wheel', function (e) {
-    e.preventDefault;
-    console.log(e.wheelDelta);
-    e.wheelDelta <= 0 ? scale -= 5 : scale += 5;
-    if (scale >= 100) {
-        scale = 100;
-    } else if (scale <= -20) {
-        scale = 200;
-        heros[slide].style.transform = `translateY(${scale}%)`;
-        slide++;
-        scale = 100;
-    };
-    heros[slide].style.transform = `translateY(${scale}%)`;
-});
+
+// const viewH = window.innerHeight;
+
+// var heros = document.querySelectorAll('.hero');
+// const heroOffset = heros[0].offsetTop
+// const offsetP = heroOffset / 100;
+// console.log(heroOffset);
+// window.addEventListener('scroll', function (e) {
+//     let offsetTop = document.documentElement.scrollTop;
+//         let percent = offsetTop / offsetP;
+//         console.log(percent);
+//         heros[0].style.transform = `translateX(${100 - percent + 5}%)`;
+// });
+
+
+
+// let scale = 100;
+// var slide = 0;
+// window.addEventListener('wheel', function (e) {
+//     e.preventDefault;
+//     console.log(e.wheelDelta);
+//     e.wheelDelta <= 0 ? scale -= 5 : scale += 5;
+//     if (scale >= 100) {
+//         scale = 100;
+//     } else if (scale <= -20) {
+//         scale = 200;
+//         heros[slide].style.transform = `translateY(${scale}%)`;
+//         slide++;
+//         scale = 100;
+//     };
+//     heros[slide].style.transform = `translateY(${scale}%)`;
+// });
